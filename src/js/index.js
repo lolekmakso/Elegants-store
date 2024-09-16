a17c13d66927acc2ef22b588e18d4fd6f78c3ce8
lolekmakso <makstarasovv1@gmail.com>
2024-09-16 17:34:20 +0300

    added swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,23 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 4.5,
+    spaceBetween: 24,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  });
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
 
 import tagline from './modules/tagline.js';
 tagline();
